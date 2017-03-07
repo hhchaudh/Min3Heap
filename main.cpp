f12f489943cae02e256cc1cd0e21faa44a5ae3e8
hhchaudh <haaris.h.chaudhry@gmail.com>
2017-03-06 20:01:15 -0600

    Added comments to Min3Heap.h and Min3Heap.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-//// EECS560_Lab1.cpp : Defines the entry polong for the console application.
-////
-//
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
@@ -8,137 +5,6 @@
 #include <sys/time.h>
 #include "Min3Heap.h"
 #include "BinarySearchTree.h"
-//
-//long getChoice();
-//void menuLoop( Min3Heap& min3Heap );
-//void insertItem( Min3Heap& min3Heap );
-//void deleteItem( Min3Heap& min3Heap );
-//void deletemin( Min3Heap& min3Heap );
-//void deletemax( Min3Heap& min3Heap );
-//void printLevelOrder( Min3Heap& min3Heap );
-//
-//int main()
-//{
-//    long anlongeger;
-//    std::ifstream fileReader( "data.txt" );
-//
-//    if( fileReader.is_open() )
-//    {
-//        Queue<long> readValues;
-//
-//        while( fileReader >> anlongeger )
-//        {
-//            readValues.enqueue( anlongeger );
-//        }
-//
-//        Min3Heap min3Heap( 200, readValues );
-//
-//        menuLoop( min3Heap );
-//        fileReader.close();
-//    }
-//    else
-//    {
-//        std::cout << "Error reading file\n";
-//    }
-//
-//    return 0;
-//}
-//
-//void menuLoop( Min3Heap& min3Heap )
-//{
-//    long choice = 0;
-//
-//    while( choice != 6 )
-//    {
-//        choice = getChoice();
-//
-//        switch( choice )
-//        {
-//        case 1:
-//            insertItem( min3Heap );
-//            break;
-//        case 2:
-//            deletemin( min3Heap );
-//            break;
-//        case 3:
-//            deletemax( min3Heap );
-//            break;
-//        case 4:
-//            deleteItem( min3Heap );
-//            break;
-//        case 5:
-//            printLevelOrder( min3Heap );
-//            break;
-//        default:
-//            if( choice != 6 )
-//            {
-//                std::cout << "Enter a valid choice (1-5)\n";
-//            }
-//            break;
-//        }
-//    }
-//}
-//
-//long getChoice()
-//{
-//    long choice = 0;
-//
-//    std::cout << "..........................................................\n";
-//    std::cout << "Please choose one of the folowing commands:\n";
-//    std::cout << "1 - insert\n"
-//        << "2 - deletemin\n"
-//        << "3 - deletemax\n"
-//        << "4 - remove\n"
-//        << "5 - levelorder\n"
-//        << "6 - exit\n"
-//        << "\n";
-//
-//    std::cin >> choice;
-//
-//    return choice;
-//}
-//
-//void insertItem( Min3Heap& min3Heap )
-//{
-//    std::cout << "Choose a number to be added to the heap\n";
-//    long number;
-//    std::cin >> number;
-//    min3Heap.insert( number );
-//}
-//
-//void deleteItem( Min3Heap& min3Heap )
-//{
-//    std::cout << "Choose a number to be removed from the heap\n";
-//    long number;
-//    std::cin >> number;
-//    if( !min3Heap.remove( number ) )
-//    {
-//        std::cout << "Error, cannot delete " << number << ", value is not in the heap.\n";
-//    }
-//}
-//
-//void deletemin( Min3Heap& min3Heap )
-//{
-//    if( min3Heap.deleteMin() == -1 )
-//    {
-//        std::cout << "The heap is empty\n";
-//    }
-//}
-//
-//void deletemax( Min3Heap& min3Heap )
-//{
-//    if( min3Heap.deleteMax() == -1)
-//    {
-//        std::cout << "The heap is empty\n";
-//    }
-//}
-//
-//void printLevelOrder( Min3Heap& min3Heap )
-//{
-//    std::cout << "Level Order:\n";
-//    min3Heap.levelOrderDisplay();
-//    std::cout << std::endl;
-//}
 
 class Timer
 {
@@ -259,9 +125,6 @@ int main()
 
     for( int i = 0; i < 4; i++ )
     {
-        // double openAverage = 0;
-        // double quadraticAverage = 0;
-        // double doubleAverage = 0;
         double heapBuildSum = 0;
         double bstBuildSum = 0;
         double heapOperationsSum = 0;
