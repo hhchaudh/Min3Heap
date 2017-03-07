75be9f7bd84be8cc25b436598a1a874a0a5187e3
hhchaudh <haaris.h.chaudhry@gmail.com>
2017-03-06 20:02:12 -0600

    Added comments to Min3Heap.h and Min3Heap.cpp

--- Min3Heap.cpp
+++ Min3Heap.cpp
@@ -1,6 +1,7 @@
 #include "Min3Heap.h"
 #include <iostream>
 
+// Simple constructor that creates an empty array of size aSize
 Min3Heap::Min3Heap( long aSize ) :
     mSIZE( aSize ),
     mNumNodes( 0 ),
@@ -8,6 +9,10 @@ Min3Heap::Min3Heap( long aSize ) :
 {
 }
 
+// This constructor is used when reading values from a file
+// Easier that using an array since we don't know how many numbers we'll read
+// First insert values as they're given, then go to the first parent and begin
+// heapifying down from the last parent to the first
 Min3Heap::Min3Heap( long aSize, Queue<long>& aQueue ) :
     mSIZE( aSize ),
     mNumNodes( 0 ),
@@ -25,6 +30,10 @@ Min3Heap::Min3Heap( long aSize, Queue<long>& aQueue ) :
     }
 }
 
+// This constructor uses an array to construct the heap
+// First insert each value in the array in order, and then
+// find the last parent and heapify downward for each parent
+// starting from the last to the first
 Min3Heap::Min3Heap( long aSize, long values[], long valuesSize ) :
     mSIZE(aSize),
     mNumNodes(0),
@@ -41,11 +50,14 @@ Min3Heap::Min3Heap( long aSize, long values[], long valuesSize ) :
     }
 }
 
+// The destructor, destroys the heap array
 Min3Heap::~Min3Heap()
 {
     delete[] mHeapArray;
 }
 
+// The search function, checks if the heap is empty before sending values to the
+// recursive search function
 long Min3Heap::search( long aSearchValue )
 {
     long foundIndex = -1;
@@ -62,12 +74,15 @@ long Min3Heap::search( long aSearchValue )
     return foundIndex;
 }
 
+// bottomUpInsert simply inserts values in the heap
+// without heapifying
 void Min3Heap::bottomUpInsert( const long aValue )
 {
     mHeapArray[mNumNodes] = aValue;
     mNumNodes++;
 }
 
+// Inserts values into the heap, and then heapifies
 void Min3Heap::insert( const long aValue )
 {
     if(mNumNodes == mSIZE)
@@ -78,31 +93,32 @@ void Min3Heap::insert( const long aValue )
     heapifyUp( aValue, mNumNodes - 1 );
 }
 
+// Removes values from the heap
 bool Min3Heap::remove( const long aValue )
 {
-    long indexOfValue = -1;
-    bool aValRemoved = false;
+    long indexOfValue = -1;         // Initial index location, used as a flag
+    bool aValRemoved = false;       // If we remove at least one value, then we switch this to true
 
-    do
-    {
-        indexOfValue = search( aValue );
+    do                              // Do at least once iteration of this loop   
+    {               
+        indexOfValue = search( aValue );    // Search for the index of aValue
 
-        if( indexOfValue == 0 )
+        if( indexOfValue == 0 )     // If it's the first value, use deleteMin()
         {
-            aValRemoved = true;
+            aValRemoved = true;     // We removed a value, return true
             deleteMin();
         }
-        else if( indexOfValue > -1 )
+        else if( indexOfValue > -1 )    // indexOfValue is greater than -1 when we found a value
         {
             aValRemoved = true;
-            mHeapArray[indexOfValue] = mHeapArray[mNumNodes - 1];
-            long valueToMove = mHeapArray[indexOfValue];
-            mHeapArray[mNumNodes - 1] = -1;
-            mNumNodes--;
+            mHeapArray[indexOfValue] = mHeapArray[mNumNodes - 1];   // Swap current index value with the last index value
+            long valueToMove = mHeapArray[indexOfValue];            // We have a new valueToMove
+            mHeapArray[mNumNodes - 1] = -1;                         // Turn the last value into a -1, indicating it's "empty"
+            mNumNodes--;                                            // Reduce the number of nodes we have in the heap
 
-            long parentIndex = getParent( indexOfValue );
+            long parentIndex = getParent( indexOfValue );           // Get the parent of the current index
 
-            if( parentIndex > -1 && ( indexOfValue != mNumNodes ) )
+            if( parentIndex > -1 && ( indexOfValue != mNumNodes ) ) // Make a decision on which way to heapify
             {
                 if( valueToMove < mHeapArray[parentIndex] )
                 {
@@ -116,16 +132,16 @@ bool Min3Heap::remove( const long aValue )
         }
     } while( indexOfValue > -1 );
 
-    return aValRemoved;
+    return aValRemoved;                                             // Return the flag indicating if a value was removed
 }
 
-void Min3Heap::levelOrderDisplay()
+void Min3Heap::levelOrderDisplay()                                  // Displays values in order
 {
     long nodeCount = 0;
 
     for( long i = 0; i < mNumNodes; i++ )
     {
-        long valuesPerLevel = pow( i );
+        long valuesPerLevel = pow( i );                             // Each level contains (3 ^ level) values
 
         for( long j = 0; j < valuesPerLevel; j++ )
         {
@@ -136,7 +152,7 @@ void Min3Heap::levelOrderDisplay()
             {
                 std::cout << "- ";
             }
-            if( nodeCount == mNumNodes )
+            if( nodeCount == mNumNodes )                            // Stop displaying if we reache the end of the heap
             {
                 std::cout << std::endl;
                 return;
@@ -147,7 +163,8 @@ void Min3Heap::levelOrderDisplay()
     }
 }
 
-long Min3Heap::deleteMin()
+// replaces the top value with the last value then heapifies
+long Min3Heap::deleteMin()                                          
 {
     if( mNumNodes > 0 )
     {
@@ -162,6 +179,9 @@ long Min3Heap::deleteMin()
     return -1;
 }
 
+// Finds the max value among the leaves
+// Deletes the value and replaces with last value
+// Check for heapification
 long Min3Heap::deleteMax()
 {
     long maxValue = -1;
@@ -200,6 +220,9 @@ long Min3Heap::deleteMax()
     return maxValue;
 }
 
+// Recursive search, check if the current value's children are 
+// greater, if so, then we should not purse further down the subtree
+// that is rooted by that child
 void Min3Heap::search( long aSearchVal, long aIndex, long & aFoundIndex )
 {
     if( aFoundIndex > -1 )
@@ -221,22 +244,14 @@ void Min3Heap::search( long aSearchVal, long aIndex, long & aFoundIndex )
             }
         }
     }
-
-//     for(long i = 0; i < mSIZE; i++)
-//     {
-//         if(mHeapArray[i] == aSearchVal)
-//         {
-//             aFoundIndex = i;
-//             return;
-//         }
-//     }
 }
 
+// Heapify downward
 void Min3Heap::heapifyDown( const long aValueToMove, const long aIndex )
 {
     long smallestChildIndex = findSmallestChildIndex( aValueToMove, aIndex );
 
-    if( smallestChildIndex > -1 )
+    if( smallestChildIndex > -1 )       // If smallestChildIndex returned -1, that means there are no smaller children
     {
         mHeapArray[aIndex] = mHeapArray[smallestChildIndex];
 
@@ -255,11 +270,12 @@ void Min3Heap::heapifyDown( const long aValueToMove, const long aIndex )
     }
 }
 
+// Heapify up
 void Min3Heap::heapifyUp( long aValueToMove, long aIndex )
 {
     long parentIndex = getParent( aIndex );
 
-    if( parentIndex > -1 )
+    if( parentIndex > -1 )                  // If the parentIndex is -1, that means there is no parent
     {
         if( aValueToMove < mHeapArray[parentIndex] )
         {
@@ -277,11 +293,13 @@ void Min3Heap::heapifyUp( long aValueToMove, long aIndex )
     }
 }
 
+// Use k*i < n-1 to check for parent status, k=3
 bool Min3Heap::isParent( long aIndex ) const
 {
     return ( ( aIndex * 3 ) < ( mNumNodes - 1 ) );
 }
 
+// Use (i-1)/k to get parent, k=3
 long Min3Heap::getParent( long aIndex ) const
 {
     if( aIndex > 0 )
@@ -294,11 +312,13 @@ long Min3Heap::getParent( long aIndex ) const
     }
 }
 
+// Check if the aNthChild exists
 bool Min3Heap::nthChildExists( long aIndex, long aNthChild ) const
 {
     return ( ( 3 * aIndex + aNthChild ) < mNumNodes );
 }
 
+// Get the aNthChild
 long Min3Heap::getNthChild( long aIndex, long aNthChild ) const
 {
     if( nthChildExists( aIndex, aNthChild ) )
@@ -311,6 +331,7 @@ long Min3Heap::getNthChild( long aIndex, long aNthChild ) const
     }
 }
 
+// Calculates powers of 3
 long Min3Heap::pow( const long exponent ) const
 {
     if( exponent == 0 )
@@ -330,11 +351,13 @@ long Min3Heap::pow( const long exponent ) const
     }
 }
 
+// Check if the heap is empty
 bool Min3Heap::isEmpty() const
 {
     return ( mNumNodes > 0 );
 }
 
+// Find the index of the maximum value, search and compare each leaf
 long Min3Heap::findMaxIndex()
 {
     long maxValIndex = lastParentIndex() + 1;
@@ -350,11 +373,13 @@ long Min3Heap::findMaxIndex()
     return maxValIndex;
 }
 
+// Find the index of the last parent
 long Min3Heap::lastParentIndex()
 {
     return ( ( mNumNodes - 2 ) / 3 );
 }
 
+// Find the index of the smallest child
 long Min3Heap::findSmallestChildIndex( long aValueToMove, long aIndex ) const
 {
     if( isParent( aIndex ) )
@@ -383,7 +408,7 @@ long Min3Heap::findSmallestChildIndex( long aValueToMove, long aIndex ) const
             {
                 if( minValueIndex == -1 || ( childIndices[i] < childIndices[minValueIndex] ) )
                 {
-                    minValueIndex = i;
+                    minValueIndex = i;      // minValueIndex has the potential to stay at -1 here, indicating no smaller children
                 }
             }
         }
